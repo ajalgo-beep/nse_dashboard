9c2f88de046cbbcf334f7836a9708f981c42ff8a
ajalgo-beep <jainanilkumar2011@gmail.com>
2025-08-29 23:24:46 +0530

    Update nse_dashboard.py

--- nse_dashboard.py
+++ nse_dashboard.py
@@ -23,16 +23,27 @@ HEADERS = {
 st.set_page_config(page_title="AJ-Algo NSE Dasboard", page_icon=":rocket:")
 st.title("📊  AJ-Algo NSE Dasboard")
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('"C:\STRONTIUM\Stock Market\ajAlgoBG.png"')
 
 # ----------------------
 # Sidebar Controls
