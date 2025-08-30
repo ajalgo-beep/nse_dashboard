882376eb6cab04b14fd7d83cc3fd9d7cee5449dd
ajalgo-beep <jainanilkumar2011@gmail.com>
2025-08-30 09:21:58 +0530

    Update nse_dashboard.py

--- nse_dashboard.py
+++ nse_dashboard.py
@@ -192,14 +192,15 @@ with col2:
 
 #---------------------
 # Constructing the message and sending onto telegram group with chatid and token id
-payload = {
-    'chat_id': '967276571',
-    #'text': f"""``` Top 5 Gainers in NIFTY50\n{tabulate(nifty50_gain_top5, headers = 'keys', tablefmt = 'pretty')}\n\n\nTop 10 Gainers in FnO Category\n{tabulate(fno_secu_gain_top10, headers = 'keys', tablefmt = 'pretty')}\n\n\nTop 10 Gainers in all securities\n{tabulate(all_secu_gain_top10, headers = 'keys', tablefmt = 'pretty')}```""",
-    'text': f"""``` Top 5 Gainers in NIFTTY50```""",
-    'parse_mode': 'MarkdownV2'}
-tokenid = '8290350450:AAHR7QvcVTp_IMpFcHhZsgeIaNyBSGTe9Q0'
-url = f'https://api.telegram.org/bot{tokenid}/sendmessage'
-requests.post(url,data=payload,verify=False)
+if telegram_alerts == True:
+    payload = {
+        'chat_id': '967276571',
+        #'text': f"""``` Top 5 Gainers in NIFTY50\n{tabulate(nifty50_gain_top5, headers = 'keys', tablefmt = 'pretty')}\n\n\nTop 10 Gainers in FnO Category\n{tabulate(fno_secu_gain_top10, headers = 'keys', tablefmt = 'pretty')}\n\n\nTop 10 Gainers in all securities\n{tabulate(all_secu_gain_top10, headers = 'keys', tablefmt = 'pretty')}```""",
+        'text': f"""``` Top 5 Gainers in NIFTTY50```""",
+        'parse_mode': 'MarkdownV2'}
+    tokenid = '8290350450:AAHR7QvcVTp_IMpFcHhZsgeIaNyBSGTe9Q0'
+    url = f'https://api.telegram.org/bot{tokenid}/sendmessage'
+    requests.post(url,data=payload,verify=False)
 #---------------------
 
 time.sleep(refresh_time * 60)
